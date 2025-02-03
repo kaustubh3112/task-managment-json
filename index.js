79b8fc14d34f985ec84b5ad83edc64a2ecaa0ad3
kaustubh3112 <60938333+kaustubh3112@users.noreply.github.com>
2025-02-03 13:24:17 +0530

    Update index.js
    Updated json path

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router1 = jsonServer.router("users.json");
+const router2 = jsonServer.router("tasks.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
-server.use(router);
 
-server.listen(port);
+// Directly use routers without extra path prefixes
+server.use(router1);
+server.use(router2);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
